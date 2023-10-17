130bf3b7ae133b8d15399e8401b11be1f72acbac
Myk <89474773+mykcryptodev@users.noreply.github.com>
2023-10-16 20:31:41 -0400

    ignore solidity with rules

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,12 @@
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-nocheck
 /**
  * Run `build` or `dev` with `SKIP_ENV_VALIDATION` to skip env validation. This is especially useful
  * for Docker builds.
  */
 await import("./src/env.mjs");
+const rulesToProcess = [/\.m?js/, /\.(js|cjs|mjs)$/].map(String);
+const dirToIgnore = /solidity/;
 
 /** @type {import("next").NextConfig} */
 const config = {
@@ -16,6 +20,21 @@ const config = {
   i18n: {
     locales: ["en"],
     defaultLocale: "en",
+  },
+  /**
+   *
+   * @param {import('webpack').Configuration} config
+   * @param {import('next/dist/server/config-shared').WebpackConfigContext} context
+   * @returns {import('webpack').Configuration}
+   */
+  webpack: (config) => {
+    config.module.rules = config.module.rules.map((rule) => {
+      if (rule !== "..." && rulesToProcess.indexOf(String(rule.test)) > -1) {
+        rule.exclude = [dirToIgnore];
+      }
+      return rule;
+    });
+    return config;
   },
 };
 
